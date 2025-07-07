66c876853f3e5e26be03c6fc1102cef71603cd67
sreekanthp <varshisree2@gmail.com>
2025-07-07 17:52:22 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0897598ZW3D3944XUN0"
+}
+
+variable "zone_name" {
+    default = "sree84s.site"
+}
+
+variable "component" {
+
+}
+
+variable "rule_priority" {
+    
+}
